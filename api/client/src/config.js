4eb24cb1a8bf57385be38273763cca99dd78c9d6
collins mbathi <perezcoolins@gmail.com>
2022-01-11 15:28:36 +0300

    added backend string

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export  const axiosInstance = axios.create({
     
-    baseUrl : "https://crashcodecca.herokuapp.com/api/course",
+    baseUrl : "https://crashcodeacademy.herokuapp.com/api/course",
 
 });
